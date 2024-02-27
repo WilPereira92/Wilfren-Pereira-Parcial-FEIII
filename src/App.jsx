afa587f6731d3e9d3fe789b6d60bd453704f56b6
WilPereira92 <alecarreno6@gmail.com>
2024-02-26 22:10:17 -0300

    Primer-commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './components/Card'
+import Form from './components/Form'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Carga de estudiantes</h1>
+      <Form/>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,11 @@
+const Form = () => {
+  return (
+    <div>
+        <form action="#">
+            
+        </form>
+    </div>
+  )
+}
+
+export default Form
